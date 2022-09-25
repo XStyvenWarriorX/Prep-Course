6814cdc13efa074eb76059131851b825ab1be2c3
XStyvenWarriorX <113223002+XStyvenWarriorX@users.noreply.github.com>
2022-09-25 11:33:39 -0500

    cambiazoooo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 == 5;
 const nuevaMultiplicacion = 10 * 4 == 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 / 5 === 4.2;
+const nuevoModulo = 21 / 5 == 4.2;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,72 +28,68 @@ const nuevoModulo = 21 / 5 === 4.2;
 function devolverString(str) {  
   // "Return" la string provista: str
   // Tu código:
-  return 0
-}
+   return devolverstring;
+} 
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  return xy; 
+var x = 0;
+var y = 1;
+var S = y + x;
+  return S;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return x-y
-  
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return x*y
+
+  return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return x / y
+  return x / y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  return true 
-    }
-    else
-      { return alce; 
-  
+  var x = 10;
+  var y = 10;
+  if (x == y);
+  return true;}
+  { 
+    
+   return false
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return true;
-  }else 
-  { return falce;
+
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  return true;
-  }else 
-  { return falce;
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-if mayorQueCincuenta > 50;   
-   return true;
-}else 
-{ return falce;
   
 }
 
