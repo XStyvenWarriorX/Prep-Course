c092cb603fb356b5e12b1124e9499ae818d04e66
XStyvenWarriorX <113223002+XStyvenWarriorX@users.noreply.github.com>
2022-11-23 12:11:03 -0500

    Volvi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -173,14 +173,15 @@ function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
 
- 
+ return Math.ceil(num);
+
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
   
-  return Math.random(x);
+  return Math.random();
 
 }
 
